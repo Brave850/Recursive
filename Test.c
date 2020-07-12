f65ccd9d1aa39866fada3984dcc3daa9aec8de6f
Brave850 <67492957+Brave850@users.noreply.github.com>
2020-07-12 20:39:15 +0530

    update prime
    This recursion program prints whether the program is prime or not.

--- Test.c
+++ Test.c
@@ -1,18 +1,29 @@
 #include<stdio.h>
-sum(int n);
+
+int prime(int n,int i);
 int main()
 {
 	int num,result;
 	printf("Enter the number: ");
 	scanf("%d",&num);
-	result=sum(num);
-	printf("Sum of digit is: %d\n",result);
+	result=prime(num,num/2);
+	if(result==1)
+	printf("\n %d is a prime number",num);
+	else
+	printf("\n %d is not a prime number",num);
 	return 0;
 }
-sum(int n)
+ int prime(int n,int i)
 {
-	if(n!=0)
-		return (n%10+sum(n/10));
-	else
+	if(i==1)
+		return 1;
+	else if(n==1)
 		return 0;
+	else
+	{
+		if(n%i==0)
+			return 0;
+		else
+			return prime(n,i-1);
+	}
 }
